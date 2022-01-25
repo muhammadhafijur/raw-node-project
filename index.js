4b9fcd041a275aaa4fe8789a6f0d578c59d1df7f
MuhammadHafijur <hafijur.salah@gmail.com>
2022-01-25 16:01:02 +0600

    parsing request

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 // dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
 
 // app object - module scaffolding
 const app = {};
@@ -27,8 +29,28 @@ app.createServer = () => {
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-  // response handle
-  res.end("Hello world");
+  // request handling
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+\/+$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+    // response handle
+    res.end("Hello world");
+  });
 };
 
 // start the server
