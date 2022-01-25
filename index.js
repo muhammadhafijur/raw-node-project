9676767d9a40d7428594addaa36bb39f08ed2429
MuhammadHafijur <hafijur.salah@gmail.com>
2022-01-25 17:04:06 +0600

    Refactor code

--- index.js
+++ index.js
@@ -8,8 +8,7 @@
 
 // dependencies
 const http = require("http");
-const url = require("url");
-const { StringDecoder } = require("string_decoder");
+const { handleReqRes } = require("./helpers/handleReqRes");
 
 // app object - module scaffolding
 const app = {};
@@ -28,30 +27,7 @@ app.createServer = () => {
 };
 
 // handle Request Response
-app.handleReqRes = (req, res) => {
-  // request handling
-  // get the url and parse it
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+\/+$/g, "");
-  const method = req.method.toLowerCase();
-  const queryStringObject = parsedUrl.query;
-  const headersObject = req.headers;
-
-  const decoder = new StringDecoder("utf-8");
-  let realData = "";
-
-  req.on("data", (buffer) => {
-    realData += decoder.write(buffer);
-  });
-
-  req.on("end", () => {
-    realData += decoder.end();
-    console.log(realData);
-    // response handle
-    res.end("Hello world");
-  });
-};
+app.handleReqRes = handleReqRes;
 
 // start the server
 app.createServer();
