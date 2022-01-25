d38e2fac0859a52069cccd628e4e4a750a0085bf
MuhammadHafijur <hafijur.salah@gmail.com>
2022-01-25 13:07:43 +0600

    Getting started with raw-node-project

---
+++ index.js
@@ -0,0 +1,21 @@
+// Uptime Monitoring Application
+
+// dependencies
+const http = require("http");
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {};
+
+// create server
+app.createServer = () => {
+  const server = http.createServer();
+};
+
+// handle Request Response
+app.handleReqRes = () => {
+  // response handle
+  res.send("Hello world");
+};
